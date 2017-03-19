992583698f47ea960f4baeb29a7848378d8af36e
tyeon95 <wony_95@hotmail.com>
2017-03-20 01:15:57 +0700

    setup done

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,5 @@
 
 class Post < ApplicationRecord
   has_many :comments
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@
 
 class User < ApplicationRecord
   has_many :comments
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
